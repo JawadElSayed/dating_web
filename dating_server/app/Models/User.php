78ea16f5d6bbad8e511c3bc1c91b11e93fc54bc2
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-04 00:46:55 +0300

    create dating server

---
+++ dating_server/app/Models/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable {
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'gender',
+        'interested_in',
+        'DOB',
+        'Latitude',
+        'Longitude',
+        'visible',
+        'password',
+        'profile_img',
+        'bio',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ dating_server/database/migrations/2022_10_03_201453_create_blockes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blockes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("blocker_id")->constrained("users");
+            $table->foreignId("blocked_id")->constrained("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blockes');
+    }
+};
